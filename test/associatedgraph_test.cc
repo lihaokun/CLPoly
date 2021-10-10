9000f4848dd21d8d67e6ccc1887b82ba7253e526
haokun li <ker@pm.me>
2021-10-10 22:00:16 +0800

    update

--- test/associatedgraph_test.cc
+++ test/associatedgraph_test.cc
@@ -4,49 +4,89 @@
 int main(int argc, char const *argv[])
 {
     
-    int n=5;
-    std::vector<clpoly::polynomial_ZZ> F;
-    for (int i=0;i<n;++i)
-    {
-        clpoly::variable xk("x"+std::to_string(i+1));
-        clpoly::variable xk1("x"+std::to_string(i+2));
-        clpoly::variable xk2("x"+std::to_string(i+3));
-        clpoly::variable xk3("x"+std::to_string(i+4));
+    // int n=5;
+    // std::vector<clpoly::polynomial_ZZ> F;
+    // for (int i=0;i<n;++i)
+    // {
+    //     clpoly::variable xk("x"+std::to_string(i+1));
+    //     clpoly::variable xk1("x"+std::to_string(i+2));
+    //     clpoly::variable xk2("x"+std::to_string(i+3));
+    //     clpoly::variable xk3("x"+std::to_string(i+4));
         
-        clpoly::polynomial_ZZ f1=xk*xk3-xk1*xk2;
-        F.push_back(f1);
-    }
-    auto G3=clpoly::associatedgraph(F);
-    std::cout<<clpoly::graph_diff_score(chordal_completion(G3),G3)<<std::endl;
-    std::cout<<clpoly::graph_diff_score(connected_branch_graph(G3),G3)<<std::endl; 
-    std::vector<clpoly::variable> l={
-       clpoly::variable("x1"),
-       clpoly::variable("x2"),
-       clpoly::variable("x3"),
-       clpoly::variable("x8"),
-       clpoly::variable("x7"),
-       clpoly::variable("x6"),
-       clpoly::variable("x4"),
-       clpoly::variable("x5")
-    };
+    //     clpoly::polynomial_ZZ f1=xk*xk3-xk1*xk2;
+    //     F.push_back(f1);
+    // }
+    // auto G3=clpoly::associatedgraph(F);
+    // std::cout<<clpoly::graph_diff_score(chordal_completion(G3),G3)<<std::endl;
+    // std::cout<<clpoly::graph_diff_score(connected_branch_graph(G3),G3)<<std::endl; 
+    // std::vector<clpoly::variable> l={
+    //    clpoly::variable("x1"),
+    //    clpoly::variable("x2"),
+    //    clpoly::variable("x3"),
+    //    clpoly::variable("x8"),
+    //    clpoly::variable("x7"),
+    //    clpoly::variable("x6"),
+    //    clpoly::variable("x4"),
+    //    clpoly::variable("x5")
+    // };
 
-    std::cout<<clpoly::elimination_height(G3,l)<<std::endl;
-    auto md=__polynomial_m_d(F,l);
-    std::cout<<md.first<<" "<<md.second<<std::endl;
+    // std::cout<<clpoly::elimination_height(G3,l)<<std::endl;
+    // auto md=__polynomial_m_d(F,l);
+    // std::cout<<md.first<<" "<<md.second<<std::endl;
 
     
-    l=std::vector<clpoly::variable>();
-    for (auto index=1;index<=10;++index)
-        l.push_back(clpoly::variable("x"+std::to_string(index)));
-    std::cout<<l<<std::endl;
-    F=clpoly::random_polynomials<clpoly::ZZ>(l,2,0.2,0.5,10,-10);
-    std::cout<<F<<std::endl;
-    auto G=clpoly::associatedgraph(F);
-    std::cout<<chordal_completion(G)<<std::endl;
-    l=clpoly::chordal_completion(G3,G);
-    std::cout<<l<<std::endl;
-    std::cout<<G3<<std::endl;
-    std::cout<<clpoly::graph_diff_score(G3,G)<<std::endl;
-    std::cout<<clpoly::graph_diff_score(clpoly::connected_branch_graph(G),G)<<std::endl;
+    // l=std::vector<clpoly::variable>();
+    // for (auto index=1;index<=10;++index)
+    //     l.push_back(clpoly::variable("x"+std::to_string(index)));
+    // std::cout<<l<<std::endl;
+    // F=clpoly::random_polynomials<clpoly::ZZ>(l,2,0.2,0.5,10,-10);
+    // std::cout<<F<<std::endl;
+    // auto G=clpoly::associatedgraph(F);
+    // std::cout<<chordal_completion(G)<<std::endl;
+    // l=clpoly::chordal_completion(G3,G);
+    // std::cout<<l<<std::endl;
+    // std::cout<<G3<<std::endl;
+    // std::cout<<clpoly::graph_diff_score(G3,G)<<std::endl;
+    // std::cout<<clpoly::graph_diff_score(clpoly::connected_branch_graph(G),G)<<std::endl;
+    
+    auto a=clpoly::variable("a");
+    auto t=clpoly::variable("t");
+    auto x=clpoly::variable("x");
+    auto y=clpoly::variable("y");
+    auto h=clpoly::variable("h");
+    auto s=clpoly::variable("s");
+    auto b=clpoly::variable("b");
+    auto c=clpoly::variable("c");
+    auto r=clpoly::variable("r");
+    
+    // clpoly::polynomial_ZZ f1=t*t-2*x*t+x*x+4*y*y-80*y+396;
+    // clpoly::polynomial_ZZ f2=5*a*a*t*t-2*a*t*x-8*a*t*y+x*x+4*y*y-4;
+    clpoly::polynomial_ZZ f0=a*a*h*h-4*s*(s-a)*(s-b)*(s-c);
+    clpoly::polynomial_ZZ f1=2*r*h-b*c;
+    clpoly::polynomial_ZZ f2=2*s-a-b-c;
+    
+    clpoly::polynomial_ZZ f3=a;
+    clpoly::polynomial_ZZ f4=b;
+    clpoly::polynomial_ZZ f5=c;
+    clpoly::polynomial_ZZ f6=r;
+    clpoly::polynomial_ZZ f7=h;
+    clpoly::polynomial_ZZ f10=a+b-c;
+    clpoly::polynomial_ZZ f11=b+c-a;
+    clpoly::polynomial_ZZ f12=-b+c+a;
+    
+    
+    
+    std::vector<clpoly::polynomial_ZZ> F={
+        f0,
+        f1,
+        f2,f3,f4,f5,f6,f7,f10,f11
+    };
+    std::cout<<F;
+    
+    auto G3=clpoly::associatedgraph(F);
+    std::cout<<G3;
+    
+    
+    
     return 0;
 }
--- test/polynomial_charset_test.cc
+++ test/polynomial_charset_test.cc
@@ -4,38 +4,64 @@
 int main(int argc, char const *argv[])
 {
     
-       clpoly::variable x1("x1"),x2("x2"),x3("x3"),x4("x4");
-    clpoly::lex_<clpoly::custom_var_order> mo(clpoly::custom_var_order({x4,x3,x2,x1}));
-    clpoly::polynomial_ZZ p1,p2,p3,p4;
-    p1=x1*pow(x4,2)+pow(x4,2)-x1*x2*x4-x2*x4+x1*x2+3*x2;
-    p2=x1*x4+x3-x1*x2;
-    p3=x3*x4-2*pow(x2,2)-x1*x2-1;
+    //    clpoly::variable x1("x1"),x2("x2"),x3("x3"),x4("x4");
+    // clpoly::lex_<clpoly::custom_var_order> mo(clpoly::custom_var_order({x4,x3,x2,x1}));
+    // clpoly::polynomial_ZZ p1,p2,p3,p4;
+    // p1=x1*pow(x4,2)+pow(x4,2)-x1*x2*x4-x2*x4+x1*x2+3*x2;
+    // p2=x1*x4+x3-x1*x2;
+    // p3=x3*x4-2*pow(x2,2)-x1*x2-1;
     
     
-    clpoly::polynomial_<clpoly::ZZ,clpoly::lex_<clpoly::custom_var_order>> p1_(&mo),p2_(&mo),p3_(&mo);
-    std::vector<clpoly::polynomial_<clpoly::ZZ,clpoly::lex_<clpoly::custom_var_order>>> P;
-    clpoly::poly_convert(p1,p1_);
-    clpoly::poly_convert(p2,p2_);
-    clpoly::poly_convert(p3,p3_);
-    auto p4_=prem(p1_,p2_,x4);
-    auto p5_=prem(p3_,p2_,x4);
-    //std::cout<<"p1_="<<p1_<<";"<<std::endl;
-    // std::cout<<"p2_="<<p2_<<";"<<std::endl;
-    // std::cout<<"p3_="<<p3_<<";"<<std::endl;
-    // std::cout<<"p4_="<<p4_<<";"<<std::endl;
-    //std::cout<<"p5_="<<p5_<<";"<<std::endl;
-    //std::cout<<"fist_var="<<(clpoly::get_first_var(p1_))<<";"<<std::endl;
-    // std::cout<<"rank(p2_)<rank(p1_)?"<<rankcompare(p2_,p1_)<<";"<<std::endl;
-    // std::cout<<rankcompare(p3_,p2_)<<";"<<rankcompare(p2_,p3_)<<std::endl;
-    //std::cout<<"p3_ reduce p1_?"<<is_reduced(p3_,p1_)<<std::endl;
-    //std::cout<<"p1_ reduce p3_?"<<is_reduced(p1_,p3_)<<std::endl;
-    P.push_back(p1_);
-    P.push_back(p2_);
-    P.push_back(p3_);
+    // clpoly::polynomial_<clpoly::ZZ,clpoly::lex_<clpoly::custom_var_order>> p1_(&mo),p2_(&mo),p3_(&mo);
+    // p1_=p1;
+    // p2_=p2;
+    // p3_=p3;
+    // std::vector<clpoly::polynomial_<clpoly::ZZ,clpoly::lex_<clpoly::custom_var_order>>> P
+    
+    // std::vector<clpoly::polynomial_<clpoly::ZZ,clpoly::lex_<clpoly::custom_var_order>>> P;
+    // clpoly::poly_convert(p1,p1_);
+    // clpoly::poly_convert(p2,p2_);
+    // clpoly::poly_convert(p3,p3_);
+    // auto p4_=prem(p1_,p2_,x4);
+    // auto p5_=prem(p3_,p2_,x4);
+    // //std::cout<<"p1_="<<p1_<<";"<<std::endl;
+    // // std::cout<<"p2_="<<p2_<<";"<<std::endl;
+    // // std::cout<<"p3_="<<p3_<<";"<<std::endl;
+    // // std::cout<<"p4_="<<p4_<<";"<<std::endl;
+    // //std::cout<<"p5_="<<p5_<<";"<<std::endl;
+    // //std::cout<<"fist_var="<<(clpoly::get_first_var(p1_))<<";"<<std::endl;
+    // // std::cout<<"rank(p2_)<rank(p1_)?"<<rankcompare(p2_,p1_)<<";"<<std::endl;
+    // // std::cout<<rankcompare(p3_,p2_)<<";"<<rankcompare(p2_,p3_)<<std::endl;
+    // //std::cout<<"p3_ reduce p1_?"<<is_reduced(p3_,p1_)<<std::endl;
+    // //std::cout<<"p1_ reduce p3_?"<<is_reduced(p1_,p3_)<<std::endl;
+    // P.push_back(p1_);
+    // P.push_back(p2_);
+    // P.push_back(p3_);
     // P.push_back(p4_);
     // P.push_back(p5_);
+    clpoly::variable c("c"),x("x"),y("y"),z("z");
+    clpoly::lex_<clpoly::custom_var_order> od(clpoly::custom_var_order({x,c,y,z}));
+    clpoly::polynomial_<clpoly::ZZ,clpoly::lex_<clpoly::custom_var_order>> f1(&od),f2(&od),f3(&od),f4(&od);
+    f1=1-c*x-x*y*y-x*z*z;
+    f2=1-c*y-y*pow(x,2)-y*pow(z,2);
+    f3=1-c*z-z*pow(x,2)-z*pow(y,2);
+    f4=8*pow(c,6)+378*pow(c,3)-27;
+    std::cout<<f1<<std::endl;
+    std::cout<<f2<<std::endl;
+    std::cout<<f3<<std::endl;
+    std::cout<<f4<<std::endl;
+    
+    std::vector<clpoly::polynomial_<clpoly::ZZ,clpoly::lex_<clpoly::custom_var_order>>> P={f1,f2,f3,f4};
     auto C=clpoly::charset(P); 
     std::cout<<C<<std::endl;
-    return 0;
+    for(auto &i:P)
+    {
+        auto f=i;
+        for(auto c=C.rbegin();c<C.rend();c++)
+        {
+            f=prem(f,*c,get_first_var(*c),false);
+        }
+        std::cout<<f<<std::endl;
+    }
     return 0;
 }
